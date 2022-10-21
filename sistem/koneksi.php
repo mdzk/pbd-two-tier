41da716aa0a17fd44e87665cee831cdb948170a6
Muhammad Dzaky <mdzaky07@gmail.com>
2022-10-21 08:27:40 +0700

    perbaikan koneksi

--- sistem/koneksi.php
+++ sistem/koneksi.php
@@ -1,10 +1,10 @@
 <?php
 function open_connection()
 {
-    $hostname = "localhost";
-    $username = "root";
+    $hostname = "103.151.63.30";
+    $username = "kelompok3";
     $password = "";
-    $dbname = "20753022_akademik";
+    $dbname = "kelompok3_akademik";
     $koneksi = mysqli_connect($hostname, $username, $password, $dbname);
     return $koneksi;
 }
